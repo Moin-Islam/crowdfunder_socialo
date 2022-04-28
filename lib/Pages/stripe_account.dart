8d2802303aa2000c44fa5e567c01976a4b1606a8
transistor-gitgud <diptox65@gmail.com>
2022-04-28 19:33:15 +0600

    eq

--- lib/Pages/stripe_account.dart
+++ lib/Pages/stripe_account.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/src/material/icons.dart';
 
 class StripeAccount extends StatefulWidget {
   const StripeAccount({Key? key}) : super(key: key);
@@ -8,6 +9,129 @@ class StripeAccount extends StatefulWidget {
 }
 
 class _StripeAccountState extends State<StripeAccount> {
+  Widget buildUserProfile() {
+    return Container(
+        child: Card(
+      child: Row(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: new Image.asset(
+              'img/person.png',
+              height: 50,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Column(
+            children: [
+              Text(
+                'Ilias Kanchon',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontWeight: FontWeight.normal),
+              ),
+              Text(
+                'User ID : 12314',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontWeight: FontWeight.normal),
+              ),
+            ],
+          )
+        ],
+      ),
+    ));
+  }
+
+  Widget buildBonusProduct() {
+    return Container(
+      width: double.infinity,
+      color: Color(0xff800080),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('This is Bonus Product'),
+              Icon(
+                Icons.turn_right,
+                color: Color(0xff800080),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Text('this is bonus product'),
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: Container(
+              height: 1.0,
+              width: double.infinity,
+              color: Colors.white,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Total Sale : 6'),
+              Text('Total Earnings : *\$6'),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildProductList() {
+    return Container(
+      width: double.infinity,
+      color: Color(0xffF4F6F8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Product 1'),
+              Icon(
+                Icons.turn_right,
+                color: Color(0xff800080),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Text('Lorem Ipsum'),
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: Container(
+              height: 1.0,
+              width: double.infinity,
+              color: Colors.black38,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('Total Sale : 6'),
+              Text('Total Earnings : *\$6'),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,37 +144,10 @@ class _StripeAccountState extends State<StripeAccount> {
                 fontSize: 20,
                 fontWeight: FontWeight.normal),
           ),
-          Container(
-            padding: EdgeInsets.all(10),
-            child: Row(
-              children: [
-                Image.asset('img/image1.jpg'),
-                Column(
-                  children: [
-                    Text(
-                      ' Hasibul Haque Prottoy',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                        fontWeight: FontWeight.normal,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text(
-                      ' User ID : 2345',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 20,
-                        fontWeight: FontWeight.normal,
-                      ),
-                    ),
-                  ],
-                )
-              ],
-            ),
+          SizedBox(
+            height: 15,
           ),
+          buildUserProfile(),
           Text(
             'Bonus Product',
             style: TextStyle(
@@ -58,60 +155,29 @@ class _StripeAccountState extends State<StripeAccount> {
                 fontSize: 20,
                 fontWeight: FontWeight.normal),
           ),
-          Container(
-              padding: EdgeInsets.all(10),
-              child: Column(
-                children: [
-                  Text(
-                    ' This is Bonus Product',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    ' Lorem Ipsum',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 20,
-                      fontWeight: FontWeight.normal,
-                    ),
-                  ),
-                  Expanded(
-                    child: new Container(
-                        margin: const EdgeInsets.only(left: 15.0, right: 10.0),
-                        child: Divider(
-                          color: Colors.black,
-                          height: 50,
-                        )),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        ' Total Sale : 6',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.normal,
-                        ),
-                      ),
-                      Text(
-                        ' Total Earnings :  *\$14.50',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.normal,
-                        ),
-                      ),
-                    ],
-                  )
-                ],
-              )),
+          buildBonusProduct(),
+          SizedBox(
+            height: 15,
+          ),
+          Text(
+            'All Product List',
+            style: TextStyle(
+                color: Color(0xff800080),
+                fontSize: 20,
+                fontWeight: FontWeight.normal),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          buildProductList(),
+          SizedBox(
+            height: 15,
+          ),
+          buildProductList(),
+          SizedBox(
+            height: 15,
+          ),
+          buildProductList(),
         ],
       ),
     );
