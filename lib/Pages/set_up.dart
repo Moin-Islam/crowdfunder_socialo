bdc95a634d935d81ed7e99dd87d0a1624707911d
transistor-gitgud <diptox65@gmail.com>
2022-06-17 09:56:29 +0600

    null safety turn off needed

--- lib/Pages/set_up.dart
+++ lib/Pages/set_up.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/src/material/flat_button.dart';
@@ -13,6 +11,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:flutter_demo/utils/token_preference.dart';
 
 class SetUp extends StatefulWidget {
   @override
@@ -45,6 +44,50 @@ class _SetUpState extends State<SetUp> {
                 fontWeight: FontWeight.normal))
       ])),
     );
+  }
+
+  bool _isLoading = false;
+
+   Future<List> fetchAddress() async {
+      final prefs = await SharedPreferences.getInstance();
+      final addressFlag = List(1);
+      addressFlag[0] = prefs.getString("name");
+      addressFlag[1] = prefs.getString("contactNumber");
+      addressFlag[2] = prefs.getString("addressLine1");
+      addressFlag[3] = prefs.getString("addressLine2");
+      return addressFlag;
+      }
+  signIn() async {
+    /*Map data = {'email': email, 'password': pass};*/
+    final prefs = await SharedPreferences.getInstance();
+    final token = prefs.getString("token");
+    print(token);
+    var dictionary = {"Authorization": token};
+    var jsonResponse = null;
+    var response = await http.post(
+      Uri.parse(
+          "https://demo.socialo.agency/crowdfunder-api-application/profile/stripeInfo"),
+          headers: 'token';
+    );
+
+    if (response.statusCode == 200) {
+      jsonResponse = json.decode(response.body);
+
+      if (jsonResponse != null) {
+        setState(() {
+          _isLoading = false;
+        });
+
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (BuildContext context) => SetUp()),
+            (Route<dynamic> route) => false);
+      }
+    } else {
+      setState(() {
+        _isLoading = false;
+      });
+      print(response.body);
+    }
   }
 
   Widget buildPublicKey() {
@@ -106,8 +149,9 @@ class _SetUpState extends State<SetUp> {
       padding: EdgeInsets.symmetric(vertical: 10),
       width: double.infinity,
       child: RaisedButton(
-        onPressed: () => Navigator.push(
-            context, MaterialPageRoute(builder: (context) => AccountSetting())),
+        onPressed: () {
+          signIn();
+        },
         padding: EdgeInsets.all(15),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
         color: Color(0xff800080),
