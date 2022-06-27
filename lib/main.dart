5640f15e683bbd9ef8f655a4292a140fc7ef303a
transistor-gitgud <diptox65@gmail.com>
2022-06-27 15:53:59 +0600

    introscreen

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.amber,
         ),
-        home: SignIn());
+        home: IntroScreen());
   }
 }
 
